0ae2bc7521bc4872c4d4b2b699667ffc939e9a58
Leah Grones <pascal.grones@gmx.at>
2024-12-01 21:46:32 +0100

    Add lib header file

--- aoc2024/day01/main.cpp
+++ aoc2024/day01/main.cpp
@@ -1,46 +1,28 @@
 #include <algorithm>
-#include <chrono>
 #include <iostream>
-#include <fstream>
 #include <string>
+#include "../lib/lib.h"
 
 using namespace std;
 
 int main(int __argc, char* argv[])
 {
+    timer timer;
     constexpr int n = 1000;
     int left[n];
     int right[n];
-
-    // compute runtime
-    auto start = chrono::high_resolution_clock::now();
-
-    // get input stream
-    string who = argv[1]; // who is either l or j
-    string file_name = "input-"s + who + ".txt"s;
-    ifstream input("day01/" + file_name);
-
-    if (input.is_open())
-    {
-        int i = 0;
-        string line;
-        
-        while (getline(input, line))
-        {
-            string delimiter = "   ";
-            right[i] = stoi(line.substr(0, line.find(delimiter)));
-            left[i] = (stoi(line.substr(line.find(delimiter) + 1, line.length())));
-            i++;
-        }
-
-        input.close();
-    }
-    else
-    {
-        cout << "Unable to open file";
-        return 1;
-    }
     
+    timer.start();
+
+    // read input
+    const auto who = argv[1]; // who is either l or j
+    read_file(who, [&right, &left](const string& line, const int i) -> void
+    {
+        const string delimiter = "   ";
+        right[i] = stoi(line.substr(0, line.find(delimiter)));
+        left[i] = stoi(line.substr(line.find(delimiter) + 1, line.length()));
+    });
+
     // sort the arrays
     sort(left, left + n);
     sort(right, right + n);
@@ -51,38 +33,27 @@ int main(int __argc, char* argv[])
     {
         result += abs(left[i] - right[i]);
     }
-
-    // compute runtime
-    auto end = chrono::high_resolution_clock::now();
-    chrono::duration<double> elapsed = end - start;
-
+    
+    timer.end();
     cout << "Result: " << result << '\n';
-    cout << "Elapsed time: " << elapsed.count() << " s\n";
 
     // PART II
-    // compute runtime
-    start = chrono::high_resolution_clock::now();
-
-    // calculate the result
+    timer.start();
+    
     result = 0;
-
-    for (int i : left)
+    for (const int i : left)
     {
         int count = 0;
-        for (int j : right)
+        for (const int j : right)
         {
             count += i == j;
         }
 
         result += i * count;
     }
-
-    // compute runtime
-    end = chrono::high_resolution_clock::now();
-    elapsed = end - start;
-
+    
+    timer.end();
     cout << "Result: " << result << '\n';
-    cout << "Elapsed time: " << elapsed.count() << " s\n";
 
     return 0;
 }
---
+++ aoc2024/lib/lib.h
@@ -0,0 +1,50 @@
+﻿#ifndef LIB_H
+#define LIB_H
+
+#include <chrono>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <functional>
+
+inline void read_file(const char* who, const std::function<void(std::string, int)>& lambda)
+{
+    const std::string file_name = std::string("input-") + who + std::string(".txt");
+    std::ifstream input("day01/" + file_name);
+
+    if (input.is_open())
+    {
+        int i = 0;
+        std::string line;
+
+        while (getline(input, line))
+        {
+            lambda(line, i++);
+        }
+
+        input.close();
+    }
+    else
+    {
+        throw std::runtime_error("Unable to open file");
+    }
+}
+
+class timer
+{
+    std::chrono::time_point<std::chrono::steady_clock> start_;
+
+public:
+    void start()
+    {
+        start_ = std::chrono::high_resolution_clock::now();
+    }
+
+    void end() const
+    {
+        const std::chrono::duration<double> elapsed = std::chrono::high_resolution_clock::now() - start_;
+        std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+    }
+};
+
+#endif
