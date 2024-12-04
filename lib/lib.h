d5824e0fab268fad2ad32899acd921767a9d81d4
Leah Grones <pascal.grones@gmx.at>
2024-12-04 20:25:03 +0100

    lib docs

--- lib/lib.h
+++ lib/lib.h
@@ -9,21 +9,29 @@
 #include <vector>
 
 namespace lib {
+
+/* Reads a file in the same folder based on `who` and applies `lambda` to each line */
 void read_file(const char *who, const std::function<void(std::string, int)> &lambda);
 
+/* Splits a string on each occurrence of `delimiter`. If the `delimiter` is `""` then a vector of single chars is returned */
 std::vector<std::string> split(const std::string s, const std::string delimiter);
 
+/* Converts a char to string */
 std::string ctos(const char c);
 
+/* Calls `func` on each element of the vector and returns the transformed vector */
 template <typename T, typename U>
 std::vector<U> map(const std::vector<T> &collection, U (*func)(T, int));
 
+/* Encapsulates a timer  */
 class timer {
    private:
     std::chrono::time_point<std::chrono::high_resolution_clock> start_;
 
    public:
+    /* Start the timer */
     void start();
+    /* Stop the timer and print the elapsed time */
     void stop() const;
 };
 
--- lib/lib.tpp
+++ lib/lib.tpp
@@ -3,13 +3,11 @@
 using std::vector;
 
 template <typename T, typename U>
-vector<U> lib::map(const vector<T> &collection, U (*func)(T, int))
-{
+vector<U> lib::map(const vector<T> &collection, U (*func)(T, int)) {
     vector<U> result;
     result.reserve(collection.size());
 
-    for (size_t i = 0; i < collection.size(); i++)
-    {
+    for (size_t i = 0; i < collection.size(); i++) {
         result.push_back(func(collection[i], i));
     }
 
