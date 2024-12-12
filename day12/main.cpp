87ea95f6196e7cb3fc8e1232f48ae7cb7c7d3173
Leah Grones <pascal.grones@gmx.at>
2024-12-12 19:58:42 +0100

    day 12: part 2 did *not* run on the first try

--- day12/main.cpp
+++ day12/main.cpp
@@ -1,12 +1,20 @@
+#include <algorithm>
 #include <unordered_set>
 
 #include "../lib/lib.h"
 
 using namespace std;
 
+struct fence {
+    int x;
+    int y;
+    char dir;
+};
+
 struct dimensions {
     int area;
     int perimeter;
+    vector<fence> fences;
 };
 
 struct coordinate {
@@ -30,40 +38,125 @@ dimensions get_dimensions(const vector<vector<char>> &garden, coordinate positio
     // Up
     if (position.y == 0 || garden[position.y - 1][position.x] != type) {
         dimensions.perimeter++;
+        dimensions.fences.push_back({position.x, position.y - 1, 'u'});
     } else {
         auto traversedPlots = get_dimensions(garden, {position.x, position.y - 1});
         dimensions.area += traversedPlots.area;
         dimensions.perimeter += traversedPlots.perimeter;
+        copy(traversedPlots.fences.begin(), traversedPlots.fences.end(), back_inserter(dimensions.fences));
     }
 
     // Right
     if (position.x == garden[0].size() - 1 || garden[position.y][position.x + 1] != type) {
         dimensions.perimeter++;
+        dimensions.fences.push_back({position.x + 1, position.y, 'r'});
     } else {
         auto traversedPlots = get_dimensions(garden, {position.x + 1, position.y});
         dimensions.area += traversedPlots.area;
         dimensions.perimeter += traversedPlots.perimeter;
+        copy(traversedPlots.fences.begin(), traversedPlots.fences.end(), back_inserter(dimensions.fences));
     }
 
     // Down
     if (position.y == garden.size() - 1 || garden[position.y + 1][position.x] != type) {
         dimensions.perimeter++;
+        dimensions.fences.push_back({position.x, position.y + 1, 'd'});
     } else {
         auto traversedPlots = get_dimensions(garden, {position.x, position.y + 1});
         dimensions.area += traversedPlots.area;
         dimensions.perimeter += traversedPlots.perimeter;
+        copy(traversedPlots.fences.begin(), traversedPlots.fences.end(), back_inserter(dimensions.fences));
     }
 
     // Left
     if (position.x == 0 || garden[position.y][position.x - 1] != type) {
         dimensions.perimeter++;
+        dimensions.fences.push_back({position.x - 1, position.y, 'l'});
     } else {
         auto traversedPlots = get_dimensions(garden, {position.x - 1, position.y});
         dimensions.area += traversedPlots.area;
         dimensions.perimeter += traversedPlots.perimeter;
+        copy(traversedPlots.fences.begin(), traversedPlots.fences.end(), back_inserter(dimensions.fences));
     }
 
     return dimensions;
+}
+
+int count_horizontal_sides_in_same_dir(vector<fence> &fences) {
+    int result = 0;
+
+    sort(fences.begin(), fences.end(), [](fence &a, fence &b) {
+        return a.y == b.y ? a.x < b.x : a.y < b.y;
+    });
+
+    do {
+        result++;
+        fence currFence = fences[0];
+        fences.erase(fences.begin());
+
+        while (!fences.empty() && currFence.y == fences[0].y && currFence.x == fences[0].x - 1) {
+            currFence = fences[0];
+            fences.erase(fences.begin());
+        }
+    } while (!fences.empty());
+
+    return result;
+}
+
+int count_vertical_sides_in_same_dir(vector<fence> &fences) {
+    int result = 0;
+
+    sort(fences.begin(), fences.end(), [](fence &a, fence &b) {
+        return a.x == b.x ? a.y < b.y : a.x < b.x;
+    });
+
+    do {
+        result++;
+        fence currFence = fences[0];
+        fences.erase(fences.begin());
+
+        while (!fences.empty() && currFence.x == fences[0].x && currFence.y == fences[0].y - 1) {
+            currFence = fences[0];
+            fences.erase(fences.begin());
+        }
+    } while (!fences.empty());
+
+    return result;
+}
+
+int count_sides(vector<fence> &fences) {
+    int result = 0;
+    vector<fence> sameDirFences;
+
+    copy_if(fences.begin(), fences.end(), back_inserter(sameDirFences), [](const fence &fence) {
+        return fence.dir == 'u';
+    });
+
+    if (!sameDirFences.empty())
+        result += count_horizontal_sides_in_same_dir(sameDirFences);
+
+    copy_if(fences.begin(), fences.end(), back_inserter(sameDirFences), [](const fence &fence) {
+        return fence.dir == 'd';
+    });
+
+    if (!sameDirFences.empty())
+        result += count_horizontal_sides_in_same_dir(sameDirFences);
+
+    copy_if(fences.begin(), fences.end(), back_inserter(sameDirFences), [](const fence &fence) {
+        return fence.dir == 'l';
+    });
+
+    if (!sameDirFences.empty())
+        result += count_vertical_sides_in_same_dir(sameDirFences);
+
+    copy_if(fences.begin(), fences.end(), back_inserter(sameDirFences), [](const fence &fence) {
+        return fence.dir == 'r';
+    });
+
+    if (!sameDirFences.empty())
+        result += count_vertical_sides_in_same_dir(sameDirFences);
+
+    return result;
 }
 
 int main(int argc, char const *argv[]) {
@@ -75,6 +168,7 @@ int main(int argc, char const *argv[]) {
         garden.push_back(plots);
     });
 
+    vector<pair<int, vector<fence>>> fences;
     int result = 0;
 
     timer.start();
@@ -83,7 +177,10 @@ int main(int argc, char const *argv[]) {
         for (int x = 0; x < garden[0].size(); x++) {
             auto dimensions = get_dimensions(garden, {x, y});
 
-            result += dimensions.area * dimensions.perimeter;
+            if (dimensions.area > 0) {
+                result += dimensions.area * dimensions.perimeter;
+                fences.push_back({dimensions.area, dimensions.fences});
+            }
         }
     }
 
@@ -97,7 +194,9 @@ int main(int argc, char const *argv[]) {
 
     timer.start();
 
-    // do stuff
+    for (auto fence : fences) {
+        result += fence.first * count_sides(fence.second);
+    }
 
     timer.stop();
 
