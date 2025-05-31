a2977cd87979ab39d19013daf76d442100c9f02c
Leah Grones <pascal.grones@gmx.at>
2025-05-31 11:27:24 +0200

    day 11: actually super simple - only took half a year to figure out

--- day11/main.cpp
+++ day11/main.cpp
@@ -1,29 +1,75 @@
 #include <math.h>
 
 #include <algorithm>
-#include <unordered_set>
+#include <unordered_map>
 
 #include "../lib/lib.h"
 
 using namespace std;
 
+tuple<long long, long long> apply_rule(const long long stone) {
+    if (stone == 0) {
+        return {1, -1};
+    }
+
+    int digits = floor(log10(stone)) + 1;
+    if (digits % 2 == 0) {
+        return {(long long)floor(stone / pow(10, digits / 2)), stone % (int)pow(10, digits / 2)};
+    }
+
+    return {stone * 2024, -1};
+}
+
 vector<long long> calculate_stones(const vector<long long>& stones) {
     vector<long long> result;
+    long long one, two;
 
     for (auto stone : stones) {
-        if (stone == 0) {
-            result.push_back(1);
-            continue;
+        tie(one, two) = apply_rule(stone);
+
+        result.push_back(one);
+
+        if (two != -1)
+            result.push_back(two);
+    }
+
+    return result;
+}
+
+long long calculate_stones_fast(const vector<long long>& initialStones) {
+    unordered_map<long long, long long> stonesByAmount;
+
+    for (auto stone : initialStones) {
+        stonesByAmount[stone]++;
+    }
+
+    vector<pair<long long, long long>> entries;
+    long long one, two;
+
+    for (size_t i = 0; i < 75; i++) {
+        for (const auto& [stone, amount] : stonesByAmount) {
+            if (amount > 0) {
+                entries.emplace_back(stone, amount);
+            }
         }
 
-        int digits = floor(log10(stone)) + 1;
-        if (digits % 2 == 0) {
-            result.push_back(floor(stone / pow(10, digits / 2)));
-            result.push_back(stone % (int)pow(10, digits / 2));
-            continue;
+        for (const auto& [stone, amount] : entries) {
+            stonesByAmount[stone] -= amount;
+
+            tie(one, two) = apply_rule(stone);
+
+            stonesByAmount[one] += amount;
+
+            if (two != -1)
+                stonesByAmount[two] += amount;
         }
 
-        result.push_back(stone * 2024);
+        entries.clear();
+    }
+
+    long long result = 0;
+    for (const auto& [_, amount] : stonesByAmount) {
+        result += amount;
     }
 
     return result;
@@ -51,68 +97,13 @@ int main(int argc, char const* argv[]) {
 
     // PART II
 
-    long result = 0;
-
     timer.start();
 
-    stones = initialStones;
-
-    // long long counts[10];
-
-    // for (long long i = 0; i < 10; i++) {
-    //     vector<long long> stones = {i};
-
-    //     for (size_t j = 0; j < 75; j++) {
-    //         stones = calculate_stones(stones);
-    //     }
-
-    //     counts[i] = stones.size();
-    // }
-
-    // for (auto item : counts) {
-    //     cout << item << ", ";
-    // }
-
-    // cout << "\n";
-
-    unordered_set<long long> skipped;
-
-    for (size_t i = 0; i < 75; i++) {
-        vector<long long> tempStones;
-
-        cout << i << ", " << stones.size() << '\n';
-        cout << *max_element(stones.begin(), stones.end()) << '\n';
-
-        for (auto stone : stones) {
-            if (stone == 0) {
-                skipped.insert(stone);
-                continue;
-            }
-
-            int digits = floor(log10(stone)) + 1;
-            if (digits % 2 == 0) {
-                // if (digits % 4 == 0) {
-                //     skipped.insert(stone);
-                //     continue;
-                // }
-
-                tempStones.push_back(floor(stone / pow(10, digits / 2)));
-                tempStones.push_back(stone % (int)pow(10, digits / 2));
-                continue;
-            }
-
-            tempStones.push_back(stone * 2024);
-        }
-
-        stones = tempStones;
-    }
-
-    vector<long long> v(skipped.begin(), skipped.end());
-    lib::print_collection(v);
+    auto result = calculate_stones_fast(initialStones);
 
     timer.stop();
 
-    cout << "Result: " << stones.size() << '\n';
+    cout << "Result: " << result << '\n';
 
     return 0;
 }
